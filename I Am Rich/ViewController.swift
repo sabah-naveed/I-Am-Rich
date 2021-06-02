cc15eceb57350b650ba79b7dbd1e8ee4238cae57
Sabah Naveed <sabahnaveed03@gmail.com>
2021-06-02 13:20:00 -0400

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Sabah Naveed on 6/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
